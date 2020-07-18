6ddb6c077b97d84a93031ab888461c9ce524b111
Rob W <robpwatkins@gmail.com>
2020-07-18 12:04:53 -0500

    getting there

---
+++ client/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Badge from './Badge';
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <Badge />
+      Let's play fetch.
+    </div>
+  )
+}
+
+export default Header;
---
+++ client/src/components/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div className="landing">
+      Heyoo, Landing.
+    </div>
+  )
+}
+
+export default Landing;
