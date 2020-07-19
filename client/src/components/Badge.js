0e982f5d8f65353f8b532e05f3e844fc3ba29a15
Rob W <robpwatkins@gmail.com>
2020-07-19 12:50:49 -0500

    that header is like. pretty good.

--- client/src/components/Badge.js
+++ client/src/components/Badge.js
@@ -3,8 +3,10 @@ import React from 'react';
 const Badge = () => {
   return (
     <div className="badge-container">
-      <div className="badge">
-        <div className="title">{/* Puppy API */}</div>
+      <div className="badge-padding">
+        <div className="badge">
+          <div className="title">Puppy API</div>
+        </div>
       </div>
     </div>
   )
