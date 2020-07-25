fa1f7c9fe991e9bbc58a0480d90956cffbd78a1b
Rob W <robpwatkins@gmail.com>
2020-07-25 15:41:00 -0500

    somethings off

--- server.js
+++ server.js
@@ -14,11 +14,12 @@ app.listen(port, () => console.log(`Listening on port ${port}...`));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  })
 }
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "build", "index.html"));
-})
 
 pool.getConnection(err => {
   if (err) throw err;
