89ce05167706dc324dcd8489fbdf8374c6904687
Rob W <robpwatkins@gmail.com>
2020-08-02 13:10:44 -0500

    making progress

--- client/src/components/Fetcher.js
+++ client/src/components/Fetcher.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Fetcher = () => {
   return (
     <div className="fetcher-container">
-      {/* <h3>Fetch a pup!</h3> */}
+      <h3>Give it a try!</h3>
       <div className="fetcher">
-        <div className="fake-input">www.puppyAPI.com/pups</div>
+        <div className="fake-input">https://www.puppyapi.com/pups/1</div>
         <button>FETCH!</button>
       </div>
     </div>
