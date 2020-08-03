00adf28d4432d186a50037d93dffffd150a851db
Rob W <robpwatkins@gmail.com>
2020-08-02 20:26:38 -0500

    and thassssss how we click outside, yall

--- client/src/components/Fetcher.js
+++ client/src/components/Fetcher.js
@@ -1,10 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
+import useOnClickOutside from '../useOnClickOutside';
 
 const Fetcher = () => {
+  const ref = useRef();
   const [fetcherActive, setFetcherActive] = useState(false);
+  useOnClickOutside(ref, () => setFetcherActive(false));
 
   return (
-    <div className={fetcherActive ? "fetcher-container active" : "fetcher-container"}>
+    <div 
+      className={
+        fetcherActive 
+        ? "fetcher-container active" 
+        : "fetcher-container"}
+      ref={ref}>
       <h3>Give it a try!</h3>
       <div className="fetcher">
         <div className="fake-input">https://www.puppyapi.com/pups/1</div>
