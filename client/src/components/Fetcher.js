0aa5ed162a597a2a9026cc9f20352d3cde7671f9
YOUR USERNAMErobpwatkins <robpwatkins@gmail.com>
2020-08-08 11:38:12 -0500

    looooove that class array trick

--- client/src/components/Fetcher.js
+++ client/src/components/Fetcher.js
@@ -5,6 +5,7 @@ import { Close } from '@material-ui/icons';
 const Fetcher = props => {
   const ref = useRef();
   const [pup, setPup] = useState([]);
+  const [isHovered, setIsHovered] = useState(false);
   useOnClickOutside(ref, () => props.setFetcherActive(false));
 
   const handleClick = () => {
@@ -12,19 +13,34 @@ const Fetcher = props => {
     getRandomPup();
   }
 
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  }
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  }
+
   const getRandomPup = async () => {
     const response = await fetch('pups/1');
     const body = await response.json();
     setPup(body);
   }
+
+  let fetcherClass = [
+    'fetcher-container',
+    props.fetcherActive && 'active',
+    isHovered && 'hovered'
+  ];
+  fetcherClass = fetcherClass.join(' ');
+
   console.log(pup);
   return (
     <div 
-      className={
-        props.fetcherActive 
-        ? "fetcher-container active" 
-        : "fetcher-container"}
-      ref={ref}>
+      className={fetcherClass}
+      ref={ref}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}>
       {!props.fetcherActive && 
       <h3>Give it a try!</h3>}
       {props.fetcherActive && pup.length > 0 &&
